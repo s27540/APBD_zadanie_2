5a80be01306934dea16df4c0d46727cb1a1b6007
s27540 <s27540@pjwstk.edu.pl>
2024-03-18 16:16:41 +0100

    Dodanie klasy Container i OverfillException

--- zadanie_2/zadanie_2/Program.cs
+++ zadanie_2/zadanie_2/Program.cs
@@ -1,3 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
+
+public class Container
+{
+    private double _loadMass;
+    private double _height;
+    private double _ownWeight;
+    private string _serialNumber;
+    private double _maxLoad;
+
+    public Container(double loadMass, double height, double ownWeight, string serialNumber, double maxLoad)
+    {
+        this._loadMass = loadMass;
+        this._height = height;
+        this._ownWeight = ownWeight;
+        this._serialNumber = serialNumber;
+        this._maxLoad = maxLoad;
+    }
+
+    public void LoadMass(double massToLoad)
+    {
+        if (massToLoad >= 0 && massToLoad <= _maxLoad)
+        {
+            _loadMass = massToLoad;
+        }
+        else
+        {
+            throw new OverfillException("Load mass must be non-negative and less than or equal to max load.");
+        }
+    }
+}
+
+
+public class OverfillException : Exception
+{ 
+    public OverfillException(string message) : base (message)
+    {
+        
+    }
+}
