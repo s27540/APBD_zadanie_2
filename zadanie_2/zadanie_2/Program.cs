87b865cbbc8b93d9653bbe30392e477cdb7bd29d
s27540 <s27540@pjwstk.edu.pl>
2024-03-22 23:38:32 +0100

    Extract RefrigeratedLoad class to another file.cs

--- zadanie_2/zadanie_2/Program.cs
+++ zadanie_2/zadanie_2/Program.cs
@@ -89,21 +89,6 @@ public class Program
     }
 }
 
-public class RefrigeratedLoad : Load
-{
-    public double _requiredTemperature { get; set; }
-
-    public RefrigeratedLoad(string name, double requiredTemperature) : base(name)
-    {
-        _requiredTemperature = requiredTemperature;
-    }
-
-    public override string ToString()
-    {
-        return $"{base.ToString()}, Required temperature: {_requiredTemperature}";
-    }
-}
-
 public class SerialNumberGenerator
 {
     private static int _defaultContainerCounter = 0;
---
+++ zadanie_2/zadanie_2/RefrigeratedLoad.cs
@@ -0,0 +1,16 @@
+﻿namespace zadanie_2;
+
+public class RefrigeratedLoad : Load
+{
+    public double _requiredTemperature { get; set; }
+
+    public RefrigeratedLoad(string name, double requiredTemperature) : base(name)
+    {
+        _requiredTemperature = requiredTemperature;
+    }
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, Required temperature: {_requiredTemperature}";
+    }
+}
