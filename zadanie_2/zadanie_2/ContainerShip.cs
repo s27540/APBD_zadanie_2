aa313c792dc4751e8065a85e2c12b7c87d7a35d1
s27540 <s27540@pjwstk.edu.pl>
2024-03-22 23:23:27 +0100

    Extract ContainerShip class to another file.cs

---
+++ zadanie_2/zadanie_2/ContainerShip.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+
+namespace zadanie_2;
+
+public class ContainerShip
+{
+    private List<Container> _loadedContainers = new List<Container>();
+    private double _maxSpeedKT;
+    private int _maxAmountOfContainers;
+    private double _maxMassOfContainersT;
+
+    public ContainerShip(double maxSpeedKt, int maxAmountOfContainers, double maxMassOfContainersT)
+    {
+        _maxSpeedKT = maxSpeedKt;
+        _maxAmountOfContainers = maxAmountOfContainers;
+        _maxMassOfContainersT = maxMassOfContainersT;
+    }
+
+    public void LoadListOfContainers(List<Container> containersToLoad)
+    {
+        
+        if (_loadedContainers.Count + containersToLoad.Count > _maxAmountOfContainers)
+        {
+            throw new InvalidOperationException("Adding these containers would exceed the maximum amount of containers allowed on the ship.");
+        }
+
+        double totalMassOfNewContainers = 0;
+        foreach (var container in containersToLoad)
+        {
+            totalMassOfNewContainers += container.getLoadMass();
+        }
+        
+        double totalMassCurrentContainers = 0;
+        foreach (var container in containersToLoad)
+        {
+            totalMassCurrentContainers += container.getLoadMass();
+        }
+
+        if (totalMassOfNewContainers + totalMassCurrentContainers > _maxMassOfContainersT * 1000)
+        {
+            throw new InvalidOperationException("Adding these containers would exceed the maximum mass of containers allowed on the ship.");
+        }
+
+        _loadedContainers.AddRange(containersToLoad);
+    }
+
+    public void LoadContainer(Container containerToLoad)
+    {
+        List<Container> containersToAdd = new List<Container> { containerToLoad };
+        LoadListOfContainers(containersToAdd);
+    }
+
+    public void RemoveContainerFromShip(Container containerToRemove)
+    {
+        _loadedContainers.Remove(containerToRemove);
+    }
+
+    public void SwitchContainers(string serialNumberToRemove, Container containerToAdd)
+    {
+        
+        for (int i = 0; i < _loadedContainers.Count; i++)
+        {
+            if (_loadedContainers[i]._serialNumber.Equals(serialNumberToRemove))
+            {
+                _loadedContainers.RemoveAt(i);
+                break;
+            }
+        }
+
+        _loadedContainers.Add(containerToAdd);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("Loaded Containers: [");
+        foreach (var container in _loadedContainers)
+        {
+            if (container != null)
+            {
+                sb.AppendLine(container.ToString());
+            }
+        }
+        sb.AppendLine($"], Max speed: {_maxSpeedKT}, Max amount of containers: {_maxAmountOfContainers}, Max mass of containers: {_maxMassOfContainersT}");
+
+        return sb.ToString();
+    }
+
+}
--- zadanie_2/zadanie_2/Program.cs
+++ zadanie_2/zadanie_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Text;
+using zadanie_2;
 
 public class Program
 {
@@ -86,92 +87,6 @@ public class Program
         Console.WriteLine($"Zawartosc kontenerowca nr 1 po przeniesieniu -> {containerShip}");
         Console.WriteLine($"Zawartosc kontenerowca nr 2 po przeniesieniu -> {containerShip2}");
     }
-}
-
-public class ContainerShip
-{
-    private List<Container> _loadedContainers = new List<Container>();
-    private double _maxSpeedKT;
-    private int _maxAmountOfContainers;
-    private double _maxMassOfContainersT;
-
-    public ContainerShip(double maxSpeedKt, int maxAmountOfContainers, double maxMassOfContainersT)
-    {
-        _maxSpeedKT = maxSpeedKt;
-        _maxAmountOfContainers = maxAmountOfContainers;
-        _maxMassOfContainersT = maxMassOfContainersT;
-    }
-
-    public void LoadListOfContainers(List<Container> containersToLoad)
-    {
-        
-        if (_loadedContainers.Count + containersToLoad.Count > _maxAmountOfContainers)
-        {
-            throw new InvalidOperationException("Adding these containers would exceed the maximum amount of containers allowed on the ship.");
-        }
-
-        double totalMassOfNewContainers = 0;
-        foreach (var container in containersToLoad)
-        {
-            totalMassOfNewContainers += container.getLoadMass();
-        }
-        
-        double totalMassCurrentContainers = 0;
-        foreach (var container in containersToLoad)
-        {
-            totalMassCurrentContainers += container.getLoadMass();
-        }
-
-        if (totalMassOfNewContainers + totalMassCurrentContainers > _maxMassOfContainersT * 1000)
-        {
-            throw new InvalidOperationException("Adding these containers would exceed the maximum mass of containers allowed on the ship.");
-        }
-
-        _loadedContainers.AddRange(containersToLoad);
-    }
-
-    public void LoadContainer(Container containerToLoad)
-    {
-        List<Container> containersToAdd = new List<Container> { containerToLoad };
-        LoadListOfContainers(containersToAdd);
-    }
-
-    public void RemoveContainerFromShip(Container containerToRemove)
-    {
-        _loadedContainers.Remove(containerToRemove);
-    }
-
-    public void SwitchContainers(string serialNumberToRemove, Container containerToAdd)
-    {
-        
-        for (int i = 0; i < _loadedContainers.Count; i++)
-        {
-            if (_loadedContainers[i]._serialNumber.Equals(serialNumberToRemove))
-            {
-                _loadedContainers.RemoveAt(i);
-                break;
-            }
-        }
-
-        _loadedContainers.Add(containerToAdd);
-    }
-
-    public override string ToString()
-    {
-        StringBuilder sb = new StringBuilder();
-        sb.AppendLine("Loaded Containers: [");
-        foreach (var container in _loadedContainers)
-        {
-            if (container != null)
-            {
-                sb.AppendLine(container.ToString());
-            }
-        }
-        sb.AppendLine($"], Max speed: {_maxSpeedKT}, Max amount of containers: {_maxAmountOfContainers}, Max mass of containers: {_maxMassOfContainersT}");
-
-        return sb.ToString();
-    }
-
 }
 
 public class Container
