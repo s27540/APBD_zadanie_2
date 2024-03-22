a12c41e58a8d8495ae909b0dbb6f17c1e218e2ef
s27540 <s27540@pjwstk.edu.pl>
2024-03-22 23:32:13 +0100

    Extract LiquidContainer class to another file.cs

---
+++ zadanie_2/zadanie_2/LiquidContainer.cs
@@ -0,0 +1,36 @@
+﻿namespace zadanie_2;
+
+public class LiquidContainer : Container, IHazardNotifier
+{
+    private bool _isDangerLoad;
+    
+    public LiquidContainer(double heightCm, double ownWeightKg, double maxLoadKg, double containerDepthCm, bool isDangerLoad) : base(heightCm, ownWeightKg, maxLoadKg, containerDepthCm)
+    {
+        _serialNumber = SerialNumberGenerator.GenerateSerialNumberForLiquidContainer();
+        _isDangerLoad = isDangerLoad;
+    }
+
+    public override void LoadMass(double massToLoad)
+    {
+        double maxCapacity = _isDangerLoad ? _maxLoadKG * 0.5 : _maxLoadKG * 0.9;
+        if (massToLoad >= 0 && massToLoad <= maxCapacity)
+        {
+            _loadMassKG = massToLoad;
+        }
+        else
+        {
+            NotifyHazard();
+        }
+    }
+    
+    public void NotifyHazard()
+    {
+        Console.WriteLine($"Hazard notification for container {_serialNumber}: Danger detected!");
+    }
+    
+    
+    public override string ToString()
+    {
+        return $"{base.ToString()}, is Danger load ?: {_isDangerLoad}";
+    }
+}
--- zadanie_2/zadanie_2/Program.cs
+++ zadanie_2/zadanie_2/Program.cs
@@ -88,40 +88,6 @@ public class Program
         Console.WriteLine($"Zawartosc kontenerowca nr 2 po przeniesieniu -> {containerShip2}");
     }
 }
-public class LiquidContainer : Container, IHazardNotifier
-{
-    private bool _isDangerLoad;
-    
-    public LiquidContainer(double heightCm, double ownWeightKg, double maxLoadKg, double containerDepthCm, bool isDangerLoad) : base(heightCm, ownWeightKg, maxLoadKg, containerDepthCm)
-    {
-        _serialNumber = SerialNumberGenerator.GenerateSerialNumberForLiquidContainer();
-        _isDangerLoad = isDangerLoad;
-    }
-
-    public override void LoadMass(double massToLoad)
-    {
-        double maxCapacity = _isDangerLoad ? _maxLoadKG * 0.5 : _maxLoadKG * 0.9;
-        if (massToLoad >= 0 && massToLoad <= maxCapacity)
-        {
-            _loadMassKG = massToLoad;
-        }
-        else
-        {
-            NotifyHazard();
-        }
-    }
-    
-    public void NotifyHazard()
-    {
-        Console.WriteLine($"Hazard notification for container {_serialNumber}: Danger detected!");
-    }
-    
-    
-    public override string ToString()
-    {
-        return $"{base.ToString()}, is Danger load ?: {_isDangerLoad}";
-    }
-}
 
 public class GasContainer : Container, IHazardNotifier
 {
