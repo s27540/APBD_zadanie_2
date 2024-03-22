133a9a74179e74d91e1ceed620f001924b42825c
s27540 <s27540@pjwstk.edu.pl>
2024-03-22 23:39:51 +0100

    Extract SerialNumberGenerator class to another file.cs

---
+++ zadanie_2/zadanie_2/SerialNumberGenerator.cs
@@ -0,0 +1,33 @@
+﻿namespace zadanie_2;
+
+public class SerialNumberGenerator
+{
+    private static int _defaultContainerCounter = 0;
+    private static int _liquidContainerCounter = 0;
+    private static int _gasContainerCounter = 0;
+    private static int _refrigeratedContainerCounter = 0;
+    
+    public static string GenerateSerialNumberForDefaultContainer()
+    {
+        _defaultContainerCounter++;
+        return $"KON-K-{_defaultContainerCounter}";
+    }
+    
+    public static string GenerateSerialNumberForLiquidContainer()
+    {
+        _liquidContainerCounter++;
+        return $"KON-L-{_liquidContainerCounter}";
+    }
+
+    public static string GenerateSerialNumberForGasContainer()
+    {
+        _gasContainerCounter++;
+        return $"KON-G-{_gasContainerCounter}";
+    }
+    
+    public static string GenerateSerialNumberForRefrigeratedContainer()
+    {
+        _refrigeratedContainerCounter++;
+        return $"KON-R-{_refrigeratedContainerCounter}";
+    }
+}
